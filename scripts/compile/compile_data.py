2eaf79ed0de5f4e9a52335b55493c1fbdd3d2099
Dan Nguyen <dansonguyen@gmail.com>
2015-08-16 16:46:28 -0700

    renamings

--- scripts/compile/compile_data.py
+++ scripts/compile/compile_data.py
@@ -2,16 +2,15 @@
 run:
     $ python3 -m scripts.compile.compile_data
 
-Simply concats all files in fetched/ and creates compiled/usgs-earthquakes.csv
+Simply concats all files in fetched/ and creates COMPILED_PATH
 """
 from scripts.settings import setup_space
-from scripts.settings import COMPILED_DIR, FETCHED_DIR
+from scripts.settings import COMPILED_PATH, FETCHED_DIR
 import os.path
 from glob import glob
 
-oname = os.path.join(COMPILED_DIR, "usgs-earthquakes.csv")
 files = glob(os.path.join(FETCHED_DIR, '*.csv'))
-with open(oname, 'w') as o:
+with open(COMPILED_PATH, 'w') as o:
     # write headers from the first file
     o.write(open(files[0]).readline())
     for fname in files:
--- scripts/settings.py
+++ scripts/settings.py
@@ -3,7 +3,7 @@ import os
 DATA_DIR = "./stash/"
 FETCHED_DIR = os.path.join(DATA_DIR, "fetched")
 COMPILED_DIR = os.path.join(DATA_DIR, "compiled")
-
+COMPILED_PATH = os.path.join(COMPILED_DIR, "usgs_earthquakes.csv")
 BASE_DATA_URL = "http://earthquake.usgs.gov/fdsnws/event/1/query.csv"
 
 
